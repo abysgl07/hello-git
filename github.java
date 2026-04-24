2e98e6d9c795bb843a3d48681a06a3072550ada7
abysgl07 <aby.sgl07@gmail.com>
2026-04-24 00:37:05 -0500

    Create github.java

---
+++ github.java
@@ -0,0 +1,12 @@
+public class github{
+    public static void main(String args[]){
+      boolean carita=false;
+
+      if(carita){
+        System.out.println("FELI"); 
+      }else{
+        System.out.println("TISTE");
+      }
+      
+    }
+}
